a40f2acb2272950dd2c80636a4aebdaf7286cdff
Giovanni <giovannibagolin@outlook.it>
2021-05-31 08:26:04 +0200

    init.

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="flex flex-row items-center justify-center w-screen h-screen">
+      <input class="w-64 h-10 rounded-lg border-2 border-black focus:" />
+      <button class="w-32 h-10 rounded-lg bg-black text-white ml-10 text-lg font-bold">Search</button>
+    </div>
+</main>
