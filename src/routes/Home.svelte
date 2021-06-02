8b158a7d912bc96e382931727420bdb2974526e7
Giovanni <giovannibagolin@outlook.it>
2021-06-02 17:48:29 +0200

    .

---
+++ src/routes/Home.svelte
@@ -0,0 +1,20 @@
+<main>
+  <div class="flex flex-col items-center h-screen">
+    <div class="mt-32 lg:mt-64 md:mt-48 sm:mt-48">
+      <h1 class="text-8xl font-black">ICE Smart Contract</h1>
+      <p class="mt-8 text-3xl text-gray-600">
+        A utility to query the ICE blockchain, to get product information by id
+      </p>
+    </div>
+    <div class="mt-16">
+      <input
+        class="w-64 h-10 rounded-lg border-2 border-black text-center text-lg italic"
+        placeholder="Product id"
+      />
+      <button
+        class="w-32 h-10 rounded-lg bg-black text-white ml-10 text-lg font-bold"
+        >Search</button
+      >
+    </div>
+  </div>
+</main>
---
+++ src/routes/Product.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let params;
+
+  const apiUrl = "";
+  let data = [];
+
+  onMount(async () => {
+    const response = await fetch(apiUrl + params.id);
+    data = await response.json();
+  });
+</script>
+
+{#if data.title || data.body === undefined}
+  loading...
+{:else}
+  <h1>{data.title}</h1>
+  <p>{data.body}</p>
+{/if}
